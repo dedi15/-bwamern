48245c718251678fa1c854613d1d427b3f8eec3c
dedi15 <aa_dedi.kurniawan@yahoo.com>
2022-06-16 11:01:09 +0700

    slicing

---
+++ src/parts/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Button from 'elements/Button';
+import BrandIcon from 'parts/IconText';
+
+export default function Header(props) {
+
+  const getNavLinkClass = path => {
+      return props.location.pathname === path ? "active" : "";
+  }
+
+  return (
+    <Header className="spacing-sm">
+        <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <BrandIcon/>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav ml-auto">
+                    <li className={`nav-item${getNavLinkClass("/")}`}>
+                        <Button className="nav-link" type="link" href="/">
+                            Home
+                        </Button>
+                    </li>
+                    <li className={`nav-item${getNavLinkClass("/browser-by")}`}>
+                        <Button className="nav-link" type="link" href="/browser-by">
+                            Browser-By
+                        </Button>
+                    </li>
+                    <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                        <Button className="nav-link" type="link" href="/stories">
+                            Stories
+                        </Button>
+                    </li>
+                    <li className={`nav-item${getNavLinkClass("/agents")}`}>
+                        <Button className="nav-link" type="link" href="/agents">
+                            Agents
+                        </Button>
+                    </li>
+                </ul>
+            </div>
+            </nav>
+        </div>
+    </Header>  
+  )
+}
